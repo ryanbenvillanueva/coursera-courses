3b6f49cf09eed40bebd6f2963e0d1cb419073907
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-05-24 16:24:50 +0800

    Pass first exercise of Programming Assignment Week 6 - Trinomial coefficients (brute force)

---
+++ computer-science-programming-with-a-purpose/week6/recursion/TrinomialDP.java
@@ -0,0 +1,23 @@
+/* *****************************************************************************
+ *  Name:              Ryan Ben S. Villanueva
+ *  Programming Assignment: Week 6 - Recursion
+ *  Last modified:     May 24, 2020
+ **************************************************************************** */
+
+public class TrinomialDP {
+
+    // Returns the trinomial coefficient T(n, k).
+    public static long trinomial(int n, int k) {
+        if (k < 0) k = -k;
+        if ()
+
+    }
+
+    // Takes two integer command-line arguments n and k and prints T(n, k).
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
+
+        StdOut.println(trinomial(n, k));
+    }
+}
